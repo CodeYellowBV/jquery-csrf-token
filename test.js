216920e4cace690ae618318ec99735b0501052ba
Damien <damien@codeyellow.nl>
2016-06-13 13:56:19 +0200

    use ava for testing

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const window = require('jsdom').jsdom().defaultView;
 const $ = require('jquery')(window);
 const C = require('./dist/jquery-csrf-token');
-const describe = require('describe');
-
 C.mockJQuery($);
+
+const test = require('ava');
 
 C.enable('TOKTOK', {
     retry: {
@@ -16,47 +16,45 @@ function mockSend(send) {
     $.ajaxTransport('+*', () => ({ send }));
 }
 
-describe('jquery-csrf-token', {
-    'add a token for each POST request'() {
-        mockSend(headers => {
-            this.expect(headers['X-CSRF-TOKEN'], 'TOKTOK');
-        });
+test('add a token for each POST request', (t) => {
+    mockSend(headers => {
+        t.is(headers['X-CSRF-TOKEN'], 'TOKTOK');
+    });
 
-        $.post({
-            url: 'test',
-            crossDomain: false, // node.js requests are detected as cross-domain by jquery
-        });
-    },
-    'no token for a GET request'() {
-        mockSend(headers => {
-            this.expect(headers['X-CSRF-TOKEN'], null);
-        });
-
-        $.get({
-            url: 'test',
-            crossDomain: false, // node.js requests are detected as cross-domain by jquery
-        });
-    },
-    'retry with new token if request return a 403'() {
-        mockSend((headers, callback) => {
-            // first request: invalid token
-            if (headers['X-CSRF-TOKEN'] === 'TOKTOK') {
-                callback(403, '', {});
-            // second request: get a new token
-            } else if (headers['X-CSRF-TOKEN'] === undefined) {
-                callback(200, 'return dat token', { token: 'fresh_token' });
-            // third request: try the same but with new token
-            } else {
-                this.expect(headers['X-CSRF-TOKEN'], 'fresh_token');
-                callback(200, 'good good good', { data: 'world on fire' });
-            }
-        });
-
-        $.post({
-            url: 'test',
-            crossDomain: false, // node.js requests are detected as cross-domain by jquery
-        });
-    },
+    $.post({
+        url: 'test',
+        crossDomain: false, // node.js requests are detected as cross-domain by jquery
+    });
 });
 
-describe.logResults();
+test('no token for a GET request', (t) => {
+    mockSend(headers => {
+        t.is(headers['X-CSRF-TOKEN'], undefined);
+    });
+
+    $.get({
+        url: 'test',
+        crossDomain: false, // node.js requests are detected as cross-domain by jquery
+    });
+});
+
+test('retry with new token if request return a 403', (t) => {
+    mockSend((headers, callback) => {
+        // first request: invalid token
+        if (headers['X-CSRF-TOKEN'] === 'TOKTOK') {
+            callback(403, '', {});
+        // second request: get a new token
+        } else if (headers['X-CSRF-TOKEN'] === undefined) {
+            callback(200, 'return dat token', { token: 'fresh_token' });
+        // third request: try the same but with new token
+        } else {
+            t.is(headers['X-CSRF-TOKEN'], 'fresh_token');
+            callback(200, 'good good good', { data: 'world on fire' });
+        }
+    });
+
+    $.post({
+        url: 'test',
+        crossDomain: false, // node.js requests are detected as cross-domain by jquery
+    });
+});
